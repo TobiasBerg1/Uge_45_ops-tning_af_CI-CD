c896822ea14d344fbfb2e56d5a17913fdc127d3e
Tobias Berg <tobiasb100@gmail.com>
2024-11-07 10:56:29 +0100

    Removed the invarians fail

--- øvelse26/Models/DomainPrimitives/Firstname.cs
+++ øvelse26/Models/DomainPrimitives/Firstname.cs
@@ -8,7 +8,7 @@ namespace Models.DomainPrimitives
 
         public Firstname(string firstName)
         {
-            //ValidateFirstName(firstName);
+            ValidateFirstName(firstName);
             _firstName = firstName;
         }
 
